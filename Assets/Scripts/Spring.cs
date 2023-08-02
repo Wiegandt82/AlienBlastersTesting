ea344e202be47f36ab8c7011e43708691e2c13a8
Wiegandt82 <wiegandt@hotmail.com>
2023-08-02 14:09:35 +0200

    Added sfx effect to spring.

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -7,18 +7,21 @@ public class Spring : MonoBehaviour
 {
     [SerializeField] Sprite _sprung;
     Sprite _defaultSprite;
+    AudioSource _audioSource;
     SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _defaultSprite = _spriteRenderer.sprite;
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
             _spriteRenderer.sprite = _sprung;
+            _audioSource.Play();
     }
 
     void OnCollisionExit2D(Collision2D collision)

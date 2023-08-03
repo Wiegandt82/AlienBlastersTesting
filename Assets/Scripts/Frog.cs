cabcefb87d857d9e256595086e944ce46e5a1919
Wiegandt82 <wiegandt@hotmail.com>
2023-08-03 15:35:40 +0200

    Adjusted frog colliders and made frog to kill the player on touch with spikes script

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class Frog : MonoBehaviour
 {
     [SerializeField] float _jumpDelay = 3;
-    [SerializeField] Vector2 _force;
+    [SerializeField] Vector2 _jumpForce;
+    [SerializeField] Sprite _jumpSprite;
 
     Rigidbody2D _rb;
     SpriteRenderer _spriteRenderer;
-    
+    Sprite _defaultSprite;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
     }
 
     void Jump()
     {
-        _rb.AddForce(_force);
+        _rb.AddForce(_jumpForce);
+        _jumpForce *= new Vector2(-1, 1);
+        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        _spriteRenderer.sprite = _jumpSprite;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        _spriteRenderer.sprite = _defaultSprite;
     }
 }
